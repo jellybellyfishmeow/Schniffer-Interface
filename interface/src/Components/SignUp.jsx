5a3316c2e1267bfa1be0f4e8c1c99cd664d2733a
Jingyu Yang <jengine.y@gmail.com>
2018-05-20 23:45:26 -0700

    starting to work on auth oh fuck

--- interface/src/Components/SignUp.jsx
+++ interface/src/Components/SignUp.jsx
@@ -7,7 +7,6 @@ export default class SignUp extends React.Component {
     constructor(props) {
         super(props);
 
-        // Default State
         this.state = {
             email: "",
             password: "",
