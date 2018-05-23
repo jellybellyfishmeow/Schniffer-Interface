6abb41975e0133a60ae07bb5b97894b5d880ca00
Jingyu Yang <jengine.y@gmail.com>
2018-05-23 12:26:45 -0700

    start working on table

--- interface/src/Components/DashboardActivity.jsx
+++ interface/src/Components/DashboardActivity.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Table} from 'react-bootstrap';
 import {Link} from "react-router-dom";
 import DChart from "./Chart";
 import SpeedTest from "./SpeedTest";
@@ -24,38 +23,7 @@ export default class DashboardActivity extends React.Component {
         <h4>You currently have <span><DevicesModal /> </span>devices on your network</h4>
                     <ThisModal />
                       <hr />                      
-                      <NetworkUsage />
-                      <hr />
-                      <Table striped bordered condensed hover>
-                          <thead>
-                          <tr>
-                            <th>Date</th>
-                            <th>Device</th>
-                            <th>Activity</th>
-                            <th>Action</th>
-                          </tr>
-                          </thead>
-                          <tbody>
-                          <tr>
-                            <td>03/01/2018</td>
-                            <td>Macbook-1</td>
-                            <td>343gb download</td>
-                            <td>check files downloaded</td>
-                          </tr>
-                          <tr>
-                            <td>02/13/2018</td>
-                            <td>iPhone-3nc5x</td>
-                            <td>3 consequtive whois sent</td>
-                            <td>check network connection</td>
-                          </tr>
-                          <tr>
-                            <td>02/09/2018</td>
-                            <td>Jennys iPhone</td>
-                            <td>10mb upload to suspicious site</td>
-                            <td>review activity</td>
-                          </tr>
-                          </tbody>
-                        </Table>
+                      <NetworkUsage />    
                         <hr />
                           <DChart />
                         <hr />
--- interface/src/Components/malicious.jsx
+++ interface/src/Components/malicious.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Table} from 'react-bootstrap';
 
 
 export default class Malicious extends React.Component {
     constructor(props) {
         super(props);
         this.state= {
-            d: new Date(1234567890).toString(), // The 0 there is the key, which sets the date to the epoch
+            d: new Date().toString(), // The 0 there is the key, which sets the date to the epoch
         }
     }
 
@@ -14,7 +15,40 @@ export default class Malicious extends React.Component {
     render() {
         return(
             <div>
-                <p>{this.state.d}</p>
+                <p>As of {this.state.d}, here are the malicious activity caught by Schniffer, 
+                sorted by date of detection</p>
+                <p>To stay up-to-date on realtime alerts, set up email alerts in your account settings!</p>
+                
+                <Table  bordered condensed hover className="table">
+                          <thead>
+                          <tr>
+                            <th>Timestamp</th>
+                            <th>IP Targeted</th>
+                            <th>Activity</th>
+                            <th>Recommended Action</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr>
+                            <td>{new Date(1526671153000).toString()}</td>
+                            <td>Macbook-1</td>
+                            <td>343gb download</td>
+                            <td>check files downloaded</td>
+                          </tr>
+                          <tr>
+                            <td>{new Date(1526269583000).toString()}</td>
+                            <td>iPhone-3nc5x</td>
+                            <td>3 consequtive whois sent</td>
+                            <td>check network connection</td>
+                          </tr>
+                          <tr>
+                            <td>{new Date(1526111122000).toString()}</td>
+                            <td>Jennys iPhone</td>
+                            <td>10mb upload to suspicious site</td>
+                            <td>review activity</td>
+                          </tr>
+                          </tbody>
+                        </Table>
             </div>
         );
     }
